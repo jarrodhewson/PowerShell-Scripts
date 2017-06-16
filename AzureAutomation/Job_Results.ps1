017420d9afda05e6230b0eab64c0a90c5f98e22b
jarrodhewson <jarrod@hewson.co.nz>
2017-06-16 12:36:52 +1200

    Updated Job_Results.ps1 in Azure Automation.

--- AzureAutomation/Job_Results.ps1
+++ AzureAutomation/Job_Results.ps1
@@ -19,21 +19,22 @@ $Output = ""
 foreach($runbook in $Runbooks){
     $YesterdayJobs = (Get-AzureRmAutomationJob -ResourceGroupName AutomationResourceGroup -AutomationAccountName LexelAutomation -RunbookName $runbook)|where starttime -gt (Get-Date).AddDays(-1)
     foreach($job in $YesterdayJobs){
-        $Output += "Runbook Name: " + $job.RunbookName + "`n"
-        $Output += "Start Time: " + $job.StartTime.DateTime + "`n"
-        $Output += "Status: " + $job.Status + "`n" + "`n"
-        $Output += "Output" + "`n"
-        $Output += "======" + "`n"
+        # Returning the Time Object wasn't giving it in NZST
+        $UTCRunbookTime = $job.StartTime.DateTime.ToUniversalTime()
+        $NZSTOffset = ([TimeZoneInfo]::FindSystemTimeZoneById("New Zealand Standard Time")).BaseUtcOffset
+        $NZSTRunbookTime = $UTCRunbookTime + $NZSTOffset
+
+        $Output += "Runbook Name: " + $job.RunbookName + "`n </br>" 
+        $Output += "Start Time: " + $NZSTRunbookTime + " (NZST)`n </br>"
+        $Output += "Status: " + $job.Status + "`n </br>" + "`n </br>"
+        $Output += "Output" + "`n </br>"
+        $Output += "======" + "`n </br>"
         $JobOutput = (Get-AzureRmAutomationJobOutput -ResourceGroupName AutomationResourceGroup -AutomationAccountName LexelAutomation -Id $job.jobid -Stream Output)
         foreach($item in $JobOutput){
-            $Output += $item.summary + "`n"
+            $Output += $item.summary + "`n </br>"
         }    
-        $Output += "`n" + "--------------------------------------------------------------------------------" + "`n" + "`n"
+        $Output += "`n </br>" + "--------------------------------------------------------------------------------" + "`n </br>" + "`n </br>"
     }
 }
 
-
-
-
-#Send-MailMessage -To jarrod.hewson@lexel.co.nz -From jarrod.test@lexel.co.nz -Subject "Test" -Body "testing body" -UseSsl -Port 587 -Credential $SMTPCreds
-Send-MailMessage -To jarrod.hewson@lexel.co.nz -Subject "Licensing Script Output" -Body $Output -From $SMTPSender -UseSsl -Port 587 -Credential $SMTPCreds
+Send-MailMessage -To jarrod.hewson@lexel.co.nz -Subject "Licensing Script Output" -BodyAsHTML $Output -From $SMTPSender -UseSsl -Port 587 -Credential $SMTPCreds
